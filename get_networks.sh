e204e52511dd71955424dc8f359feda88f798859
Antti Partanen <anttipartanen10@gmail.com>
2017-12-21 12:18:35 +0200

    Fixed the get_networks script so that it now checks if the network already exists on the computer.

--- get_networks.sh
+++ get_networks.sh
@@ -1,25 +1,65 @@
 ### wiki-Vote: 7115 nodes:
-echo Downloading the wiki-Vote network
-wget http://snap.stanford.edu/data/wiki-Vote.txt.gz
-### soc-Epinions1 75 879 nodes
-echo Downloading the soc-Epinions1 network:
-wget http://snap.stanford.edu/data/soc-Epinions1.txt.gz
-### ego-Gplus: 107 614 nodes
-echo Downloading the ego-Gplus network:
-wget http://snap.stanford.edu/data/gplus_combined.txt.gz
-### soc-Pokec: 1 632 803 nodes
-echo Downloading the soc-Pokec networks:
-wget http://snap.stanford.edu/data/soc-pokec-relationships.txt.gz
-wget http://snap.stanford.edu/data/soc-pokec-profiles.txt.gz
-### soc-LiveJournal1 : 4 847 571 nodes
-echo Downloading the soc-LiveJournal1
-wget http://snap.stanford.edu/data/soc-LiveJournal1.txt.gz
+if [ -e wiki-Vote.txt ]
+then
+	echo wiki-Vote.txt already exists\!
+else
+	echo Downloading the wiki-Vote network
+	wget http://snap.stanford.edu/data/wiki-Vote.txt.gz
+	echo Unzipping wiki-Vote.txt.gz
+	gunzip wiki-Vote.txt.gz
+	echo Done\!
+fi
 
-## Unzip the files
-echo Unzipping the files
-gunzip wiki-Vote.txt.gz
-gunzip soc-Epinions1.txt.gz
-gunzip gplus_combined.txt.gz
-gunzip soc-pokec-relationships.txt.gz
-gunzip soc-pokec-profiles.txt.gz
-gunzip soc-LiveJournal1.txt.gz
+### soc-Epinions1 75 879 nodes
+if [ -e soc-Epinions1.txt ]
+then
+	echo soc-Epinions1.txt already exitsts\!
+else
+	echo Downloading the soc-Epinions1 network:
+	wget http://snap.stanford.edu/data/soc-Epinions1.txt.gz
+	echo Unzipping soc-Epinions1.txt.gz
+	gunzip soc-Epinions1.txt.gz
+fi
+
+### ego-Gplus: 107 614 nodes
+if [ -e gplus_combined.txt ]
+then
+	echo gplus_combined.txt already exitsts\!
+else
+	echo Downloading the gplus_combined network:
+	wget http://snap.stanford.edu/data/gplus_combined.txt.gz
+	echo Unzipping gplus_combined.txt.gz
+	gunzip gplus_combined.txt.gz
+fi
+
+### soc-Pokec: 1 632 803 nodes
+if [ -e soc-pokec-relationships.txt ]
+then
+	echo soc-pokec-relationships.txt already exitsts\!
+else
+	echo Downloading the soc-pokec-relationships network:
+	wget http://snap.stanford.edu/data/soc-pokec-relationships.txt.gz
+	echo Unzipping soc-pokec-relationships.txt.gz
+	gunzip soc-pokec-relationships.txt.gz
+fi
+
+if [ -e soc-pokec-profiles.txt ]
+then
+	echo soc-pokec-profiles.txt already exitsts\!
+else
+	echo Downloading the soc-pokec-profiles network:
+	wget http://snap.stanford.edu/data/soc-pokec-profiles.txt.gz
+	echo Unzipping soc-pokec-profiles.txt.gz
+	gunzip soc-pokec-profiles.txt.gz
+fi
+
+### soc-LiveJournal1 : 4 847 571 nodes
+if [ -e soc-LiveJournal1.txt ]
+then
+	echo soc-LiveJournal1.txt already exitsts\!
+else
+	echo Downloading the soc-LiveJournal1 network:
+	wget http://snap.stanford.edu/data/soc-LiveJournal1.txt.gz
+	echo Unzipping soc-LiveJournal1.txt.gz
+	gunzip soc-LiveJournal1.txt.gz
+fi
