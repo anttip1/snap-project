8db3f681afac6c3d9b2902e72e51c07f230f890d
Antti Partanen <anttipartanen10@gmail.com>
2017-12-29 21:28:49 +0200

    This should calculate the stats for all graphs

--- network_statistics.py
+++ network_statistics.py
@@ -5,15 +5,102 @@ import networkx as nx
 from collections import Counter
 import time
 
+
 def main():
-    analyze_wiki_vote()
+    networks = ["wiki-Vote.txt", "soc-Epinions1.txt", "gplus_combined.txt"]
+    '''
+    for network in networks:
+        tic = time.clock()
+        s_nodes, s_edges = lscc(network)
+        toc = time.clock()
+        s_time = toc - tic
+        tic = time.clock()
+        w_nodes, w_edges = lwcc(network)
+        toc = time.clock()
+        w_time = toc - tic
+        print("Network: {}".format(network))
+        print("LSCC nodes: {}, LSCC edges: {}. Running time: {}".format(s_nodes, s_edges, s_time))
+        print("LWCC nodes: {}, LWCC edges: {}. Running time: {}".format(w_nodes, w_edges, w_time))
+    '''
+
+    analyze_graph("wiki-Vote.txt", True)
+    analyze_graph("wiki-Vote.txt", False)
 
 
-def analyze_wiki_vote():
 
-    tic = time.clock()
+def lscc(filename):
 
-    data = np.genfromtxt("wiki-Vote.txt", delimiter="\t", comments="#")
+    f = open(filename)
+    line = f.readline()
+
+    G = nx.DiGraph()
+
+    while line:
+        if not line.startswith("#"):
+            nodes = line.split()
+            if nodes:
+                G.add_edge(nodes[0], nodes[1])
+        line = f.readline()
+
+    largest_strongly_connected_component = max(nx.strongly_connected_components(G), key=len)
+    edge_count = 0
+
+    f.seek(0) # reset the file back to start
+    line = f.readline()
+
+    while line:
+        if not line.startswith("#"):      
+            nodes = line.split()
+            if nodes:
+                if nodes[0] in largest_strongly_connected_component and nodes[1] in largest_strongly_connected_component:
+                    edge_count += 1
+        line = f.readline()
+
+    f.close()
+
+
+    return len(largest_strongly_connected_component), edge_count
+
+def lwcc(filename):
+    f = open(filename)
+    line = f.readline()
+
+    G = nx.Graph()
+
+    while line:
+        if not line.startswith("#"):
+            nodes = line.split()
+            if nodes:
+                G.add_edge(nodes[0], nodes[1])
+        line = f.readline()
+
+    largest_weakly_connected_component = max(nx.connected_components(G), key=len)
+    edge_count = 0
+
+    f.seek(0) # reset the file back to start
+    line = f.readline()
+
+    while line:
+        if not line.startswith("#"):
+            nodes = line.split()
+            if nodes:
+                if nodes[0] in largest_weakly_connected_component and nodes[1] in largest_weakly_connected_component:
+                    edge_count += 1
+        line = f.readline()
+
+    f.close()
+
+
+    return len(largest_weakly_connected_component), edge_count
+
+    
+
+
+
+def largest_strongly_connected_component(filename):
+    # This is only for directed graphs.
+
+    data = np.genfromtxt(filename, delimiter="\t", comments="#", dtype=int)
     G = nx.DiGraph()
     G.add_edges_from(data)
     largest_strongly_connected_component = max(nx.strongly_connected_components(G), key=len)
@@ -23,25 +110,55 @@ def analyze_wiki_vote():
         if node1 in largest_strongly_connected_component and node2 in largest_strongly_connected_component:
             edge_count += 1
 
-    print("wiki-Vote.txt")
+    #print("{}".format(filename))
+    #print("\t{}\t{}".format("LSCC nodes", "LSCC edges"))
+    #print("\t{}\t{}".format(len(largest_strongly_connected_component), edge_count))
+    return len(largest_strongly_connected_component), edge_count
 
-    print("Number of nodes in wiki-Vote.txt: {}".format(len(G.nodes())))
-    print("Number of edges in wiki-Vote.txt: {}".format(len(G.edges())))
+def largest_weakly_connected_component(filename):
+    # This is only for undirected graphs.
+    data = np.genfromtxt(filename, delimiter="\t", comments="#")
+    G = nx.Graph()
+    G.add_edges_from(data)
+    largest_weakly_connected_component = max(nx.connected_components(G), key=len)
 
-    print("Number of nodes in LSCC: {}".format(len(largest_strongly_connected_component)))
-    print("Number of edges in LSCC: {}".format(edge_count))
+    edge_count = 0
+    for (node1, node2) in list(map(tuple, data)):
+        if node1 in largest_weakly_connected_component and node2 in largest_weakly_connected_component:
+            edge_count += 1
+
+    #print("{}".format(filename))
+    #print("\t{}\t{}".format("LWCC nodes", "LWCC edges"))
+    #print("\t{}\t{}".format(len(largest_weakly_connected_component), edge_count))
+    return len(largest_weakly_connected_component), edge_count
+
+def analyze_graph(filename, directed=True):
+
+    tic = time.clock()
+    G = nx.DiGraph() if directed else nx.Graph()
+
+    f = open(filename)
+    line = f.readline()
+    
+    while line:
+        if not line.startswith("#"):
+            nodes = line.split()
+            if nodes:
+                G.add_edge(nodes[0], nodes[1])
+        line = f.readline()
+
     
     # Rather than gathering all lengths between node pairs, because the lengths are integers, we can just accumulate 
     # them on an dict. The dict key denotes the lenght and the value the count of that length in shortest_path_length 
     # dict. 
     length_counts = Counter()
-
+    
     for node, other_nodes in nx.shortest_path_length(G):
         length_counts += Counter(other_nodes.values())
 
+    
     # Take the 0 away, because it just denotes the nodes length to itself.
     del length_counts[0]
-    print(length_counts)
 
     length_sum = 0
     max_length = max(length_counts, key=int)
@@ -71,6 +188,12 @@ def analyze_wiki_vote():
             break
         cumulative_count += length_counts[length]
 
+    print("network: {}".format(filename))
+    if directed:
+        print("Directed graph:")
+    else:
+        print("Undirected graph")
+
     print("1. Median distance: {}".format(median_distance))
     print("2. Mean distance: {}".format(mean_distance))
     print("3. Diameter: {}".format(diameter))
@@ -79,15 +202,6 @@ def analyze_wiki_vote():
     toc = time.clock()
 
     print("Processing time: {}s".format(toc-tic))
-
-
-    # This method, although more readable, is really slow compared to the above version.
-    # It prints out the values for edges and nodes in LSCC
-    #LSCC_graph = max(nx.strongly_connected_component_subgraphs(G), key=len)
-    #print(len(LSCC_graph))
-    #print(len(LSCC_graph.nodes()))
-    #print(len(LSCC_graph.edges()))
-
 
 
 
